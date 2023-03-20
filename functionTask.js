fb3b605af09f1da97f0a153da30e9fb52791eb4e
Issam-Addi <essam.h.addi@gmail.com>
2023-03-20 14:34:41 +0300

    new branch

--- functionTask.js
+++ functionTask.js
@@ -3,17 +3,7 @@ function userGender(){
     while(gender != "male" && gender != "female") {
         gender = prompt("Plz', enter your gender (male/female) again");
         if (gender == "male")
-        return "male";
-        else
-        return "female";
+        return gender;
     }
 }
-
-let answers = [];
-let nameuser = prompt("What is your name ?");
-let gender = userGender();let drink = prompt("Do you want to drink (hot or cold) ?");
-let drinkName = prompt("what kind of drink would you like ?");
-answers.push(nameuser, gender, drink, drinkName);
-for(let i = 0; i < answers.length ; i++) {
-    console.log(answers[i]);
-}
+let gender = userGender();
